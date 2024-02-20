c37312a530aed06d442f7f9f9a187ad9434a0c54
Mike Lee <mikemunkyu.lee@gmail.com>
2024-02-19 19:38:44 -0500

    Setup login btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import LoginButton from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p className="font-semibold">Hello!</p>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-green-600">
+      <LoginButton>
+        <Button>Login</Button>
+      </LoginButton>
+    </main>
+  );
 }
